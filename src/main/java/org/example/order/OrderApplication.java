6d7cf8cbc3d2437c6c69a6237d951cd0c4dc6393
Furkan <furkan.aydin@students.fhnw.ch>
2024-11-22 13:41:13 +0100

    OrderApplication wieder zurückgesetzt

--- src/main/java/org/example/order/OrderApplication.java
+++ src/main/java/org/example/order/OrderApplication.java
@@ -1,38 +1,14 @@
 package org.example.order;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 public class OrderApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(OrderApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
-
-    @Bean
-    @Profile("!test")
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-        return args -> {
-            Book book = restTemplate.getForObject(
-                    "http://localhost:8080/api/books/search", Book.class);
-            log.info(book.toString());
-        };
     }
 
 
